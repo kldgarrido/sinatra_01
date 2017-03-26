9e673588f7e369616f28179779168514235977b0
kldgarrido <kldgarrido@gmail.com>
2017-03-26 06:29:12 -0500

    change request

--- solution.rb
+++ solution.rb
@@ -1,17 +1,13 @@
 require 'sinatra'
 
-get '/:nombre' do
-	
 
+get '/' do
 	if params[:nombre].empty?
 		str_nombre = 'desconocido'
 	else
 		str_nombre = params[:nombre]
 	end
 	
-	"<h1>!Hola #{str_nombre}!</h1>"
+	"<h1>Hola #{str_nombre}!</h1>"s
 end
 
-get '/' do
-	"<h1>!Hola desconocido!</h1>"
-end
